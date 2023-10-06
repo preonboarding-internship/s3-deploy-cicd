b3293e0cb26e3e00731898adae9230b7b007c770
Yeonuk Hwang <younuk23@gmail.com>
2023-10-06 16:54:11 +0900

    FEAT: change title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello, AWS CLI</h1>
+        <h1>Hello, Deployment</h1>
       </header>
     </div>
   );
